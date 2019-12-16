b3715b3f31ee4453f6ed2ada290c7b6bf4fda87f
Mihai Pomarlan <mpomarlan@yahoo.co.uk>
2019-12-16 15:24:28 +0100

    Updated ecv generation script to reflect the moved concepts.

--- scripts/ecv.sh
+++ scripts/ecv.sh
@@ -7,5 +7,5 @@
 #__maintainer__ = "Mihai Pomarlan"
 #__email__ = "pomarlan@uni-bremen.de"
 
-python ./elan_cv.py -of EASE.ecv -if ../owl/EASE.owl ../owl/EASE-ACT.owl -vi "lvl1-ease_tsd-motion" -ns "http://www.ease-crc.org/ont/EASE-ACT.owl#" -sc ManipulationAction PosturalAction Taxis Actuating -vi "lvl_2-ease_tsd-task_step" -ns 'http://www.ease-crc.org/ont/EASE-ACT.owl#' -sc PhysicalTask -vi "think-aloud_coding" -ns "http://www.ease-crc.org/ont/EASE-ACT.owl#" -sc ThinkAloudTopic
+python ./elan_cv.py -of EASE.ecv -if ../owl/EASE.owl ../owl/EASE-OBJ.owl ../owl/EASE-PROC.owl ../owl/EASE-STATE.owl ../owl/EASE-ACT.owl -vi "lvl1-ease_tsd-motion" -ns "http://www.ease-crc.org/ont/EASE-PROC.owl#" -sc Locomotion BodyMovement -vi "lvl1-ease_tsd-motion" -ns "http://www.ease-crc.org/ont/EASE-ACT.owl#" -sc Manipulating Actuating -vi "lvl_2-ease_tsd-task_step" -ns 'http://www.ease-crc.org/ont/EASE-ACT.owl#' -sc PhysicalTask -vi "think-aloud_coding" -ns "http://www.ease-crc.org/ont/EASE-ACT.owl#" -sc ThinkAloudTopic
 
