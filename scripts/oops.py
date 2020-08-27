97183f280d86d27b78965b0b194e7122150c49aa
Daniel Beßler <danielb@uni-bremen.de>
2020-08-27 23:31:48 +0200

    github action

--- scripts/oops.py
+++ scripts/oops.py
@@ -9,7 +9,7 @@ OOPS_URL="http://oops.linkeddata.es/rest"
 def oops(owl_file):
 	#
 	f = open(owl_file, 'r')
-	content = f.read()
+	content = f.read().encode('utf-8')
 	f.close()
 	#
 	xml_content = """
