06ba6239c5d254199c1b4e693ec962cdf5296317
Daniel Beßler <danielb@uni-bremen.de>
2020-08-28 01:20:05 +0200

    github action

--- scripts/oops-github.py
+++ scripts/oops-github.py
@@ -3,6 +3,12 @@
 import sys
 import xml.etree.ElementTree as ET
 
+def get_affected_soma_iris(xml_elem):
+	affects = xml_elem.findall('{http://www.oeg-upm.net/oops}Affects')[0]
+	iris = list(map(lambda x: x.text,
+		affects.findall('{http://www.oeg-upm.net/oops}AffectedElement')))
+	return list(filter(lambda x: "SOMA.owl" in x, iris))
+
 def format_pitfall(name,descr,iris):
 	return descr+" Affected resources: "+str(list(map(lambda x: x.split('#')[1],iris)))
 
@@ -26,14 +32,12 @@ if __name__ == "__main__":
 		name  = pitfall_xml.findall('{http://www.oeg-upm.net/oops}Name')[0].text
 		descr = pitfall_xml.findall('{http://www.oeg-upm.net/oops}Description')[0].text
 		level = pitfall_xml.findall('{http://www.oeg-upm.net/oops}Importance')[0].text
-		affects = pitfall_xml.findall('{http://www.oeg-upm.net/oops}Affects')[0]
-		iris = list(map(lambda x: x.text, affects.findall('{http://www.oeg-upm.net/oops}AffectedElement')))
+		iris = get_affected_soma_iris(pitfall_xml)
 		report_pitfall(name,descr,level,iris)
 	
 	for suggestion_xml in root.findall('{http://www.oeg-upm.net/oops}Suggestion'):
 		name  = suggestion_xml.findall('{http://www.oeg-upm.net/oops}Name')[0].text
 		descr = suggestion_xml.findall('{http://www.oeg-upm.net/oops}Description')[0].text
-		affects = suggestion_xml.findall('{http://www.oeg-upm.net/oops}Affects')[0]
-		iris = list(map(lambda x: x.text, affects.findall('{http://www.oeg-upm.net/oops}AffectedElement')))
+		iris = get_affected_soma_iris(suggestion_xml)
 		report_suggestion(name,descr,iris)
 
