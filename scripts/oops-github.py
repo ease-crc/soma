3743a8ba103bd60e3ff356c719c5da3abf7e458a
Daniel Beßler <danielb@uni-bremen.de>
2020-08-28 02:07:11 +0200

    github action

--- scripts/oops-github.py
+++ scripts/oops-github.py
@@ -26,12 +26,8 @@ def get_affected_soma_iris(xml_elem):
 def get_resource_names(iris):
 	return list(map(lambda x: x.split('#')[1],iris))
 
-def format_pitfall(name,descr,names):
-	return descr+" Affected resources: "+str(names)
-
 def report_pitfall(name,descr,level,iris):
 	names = get_resource_names(iris)
-	#msg = format_pitfall(name,descr,names)
 	if level=="Important":
 		msg_level="warning"
 	elif level=="Minor":
@@ -56,13 +52,19 @@ if __name__ == "__main__":
 	xml_file = sys.argv[1]
 	root = ET.parse(xml_file).getroot()
 	
+	pitfalls = {"Important": [], "Minor": []}
 	for pitfall_xml in root.findall('{http://www.oeg-upm.net/oops}Pitfall'):
 		name  = pitfall_xml.findall('{http://www.oeg-upm.net/oops}Name')[0].text
 		descr = pitfall_xml.findall('{http://www.oeg-upm.net/oops}Description')[0].text
 		level = pitfall_xml.findall('{http://www.oeg-upm.net/oops}Importance')[0].text
 		iris = get_affected_soma_iris(pitfall_xml)
 		if len(iris)>0:
-			report_pitfall(name,descr,level,iris)
+			pitfalls[level].append((name,descr,iris))
+	
+	for (name,descr,iris) in pitfalls["Important"]:
+		report_pitfall(name,descr,"Important",iris)
+	for (name,descr,iris) in pitfalls["Minor"]:
+		report_pitfall(name,descr,"Minor",iris)
 	
 	for suggestion_xml in root.findall('{http://www.oeg-upm.net/oops}Suggestion'):
 		name  = suggestion_xml.findall('{http://www.oeg-upm.net/oops}Name')[0].text
