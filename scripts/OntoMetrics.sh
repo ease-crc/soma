af76bc427b12416f39c87c4597b2908bb814fa3c
Sascha Jongebloed <sasjonge@uni-bremen.de>
2022-03-28 11:32:05 +0200

    fixed ontometrics

--- scripts/OntoMetrics.sh
+++ scripts/OntoMetrics.sh
@@ -7,34 +7,23 @@ BASEDIR=$(dirname "$0")
 # The local OWL file
 OWL_FILE="$1"
 # The server URL
-HOST="https://ontometrics.informatik.uni-rostock.de/ontologymetrics/ServletController"
+HOST="http://opi.informatik.uni-rostock.de/api"
 
 echo "${BASEDIR}"
 
-# Prints urldata to stdin
-function get_data {
-	echo -n "text="
-	php -r "echo rawurlencode(file_get_contents('${1}'));"
-	echo "&path=&base=on"
-}
-
 # the web-service return HTML with a link to XML data
 echo "fetching result from https://ontometrics.informatik.uni-rostock.de"
-XML_URL=`get_data ${OWL_FILE} | curl ${HOST} \
-	-H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0' \
-	-H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' \
-	-H 'Accept-Language: en-US,en;q=0.5' \
-	--compressed \
-	-H 'Content-Type: application/x-www-form-urlencoded' \
-	-H 'Origin: https://ontometrics.informatik.uni-rostock.de' \
+echo "@${OWL_FILE}"
+curl ${HOST} \
+	-H 'Server: Apache/2.4.38 (Debian)' \
+	-H 'saved: true' \
+	-H 'Content-Type: application/xml' \
+	-H 'Vary: Accept-Encoding' \
+	-H 'Content-Encoding: gzip' \
+	-H 'Keep-Alive: timeout=5, max=100' \
 	-H 'Connection: keep-alive' \
-	-H 'Referer: https://ontometrics.informatik.uni-rostock.de/ontologymetrics/' \
-	-H 'Upgrade-Insecure-Requests: 1' \
-	--data "@-" | egrep -o 'https?://[^ ]+\.xml'`
+	--data "@${OWL_FILE}"  > $2/metrics.xml
 
-# download XML data
-echo "writing metrics.xml"
-curl ${XML_URL} > "$2/metrics.xml"
 # convert to JSON
 echo "writing metrics.json"
 ${BASEDIR}/xml2json.py -t xml2json -o "$2/metrics.json" "$2/metrics.xml"
