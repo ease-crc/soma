0ac3d92e8bf3d3ac2007b0073c6463c90b412775
Robin Nolte <mnolte@uni-bremen.de>
2022-05-24 14:57:39 +0200

    CI stuff

--- .github/actions/IsDefinedInAdder/src/main/java/IsDefinedInAdder.java
+++
@@ -1,9 +0,0 @@
-import org.springframework.stereotype.Component;
-
-@Component
-public class IsDefinedInAdder {
-
-
-
-	public void
-}
--- .github/actions/IsDefinedInAdder/src/main/java/OntologyProperties.java
+++
@@ -1,8 +0,0 @@
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-import java.nio.file.Path;
-
-@ConfigurationProperties(prefix = "ontology")
-record OntologyProperties(Path directory) {
-
-}
