436c6b5806ea7f65a9498cf9e9af66178b6e431d
Sascha Jongebloed <sasjonge@uni-bremen.de>
2022-07-21 16:26:36 +0200

    added version info

--- scripts/java/src/main/java/main/Collapser.java
+++ scripts/java/src/main/java/main/Collapser.java
@@ -1,6 +1,7 @@
 package main;
 
 import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.AddOntologyAnnotation;
 import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyStorageException;
@@ -8,6 +9,7 @@ import org.semanticweb.owlapi.model.parameters.Imports;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -18,42 +20,50 @@ import java.io.IOException;
 @Priority(Integer.MAX_VALUE - 1)
 public class Collapser implements CommandLineRunner {
 
-	/**
-	 * {@link Logger} of this class.
-	 */
-	private static final Logger LOGGER = LoggerFactory.getLogger(Collapser.class);
-	private final OntologyManager ontologyManager;
-	private final OntologyConfig ontologyConfig;
+    /**
+     * {@link Logger} of this class.
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(Collapser.class);
+    private final OntologyManager ontologyManager;
+    private final OntologyConfig ontologyConfig;
 
-	@Autowired
-	public Collapser(final OntologyManager ontologyManager, final OntologyConfig ontologyConfig) {
-		this.ontologyManager = ontologyManager;
-		this.ontologyConfig = ontologyConfig;
-	}
+    @Value("${versionInfo}")
+    private String versionInfo;
 
-	@Override
-	public void run(final String... args)
-			throws OWLOntologyStorageException, OWLOntologyCreationException, IOException {
-		final var collapseManager = OWLManager.createOWLOntologyManager();
-		for (final CollapseConfig collapseConfig : ontologyConfig.toCollapse()) {
-			final var toCollapse = ontologyManager.getOntologyManager().ontologies()
-			                                      .filter(collapseConfig::isReferenceOf).findFirst();
+    @Autowired
+    public Collapser(final OntologyManager ontologyManager, final OntologyConfig ontologyConfig) {
+        this.ontologyManager = ontologyManager;
+        this.ontologyConfig = ontologyConfig;
+    }
 
-			if (toCollapse.isEmpty()) {
-				throw new RuntimeException("Cannot resolve " + collapseConfig);
-			}
+    @Override
+    public void run(final String... args)
+            throws OWLOntologyStorageException, OWLOntologyCreationException, IOException {
+        final var collapseManager = OWLManager.createOWLOntologyManager();
+        for (final CollapseConfig collapseConfig : ontologyConfig.toCollapse()) {
+            final var toCollapse = ontologyManager.getOntologyManager().ontologies()
+                    .filter(collapseConfig::isReferenceOf).findFirst();
 
-			@SuppressWarnings("OptionalGetWithoutIsPresent") final var collapsed = collapseManager.createOntology(
-					toCollapse.get().getOntologyID().getOntologyIRI().get());
+            if (toCollapse.isEmpty()) {
+                throw new RuntimeException("Cannot resolve " + collapseConfig);
+            }
 
-			LOGGER.info("Collapsing {}", collapseConfig.ontology());
-			collapsed.addAxioms(toCollapse.get().axioms(Imports.INCLUDED));
+            @SuppressWarnings("OptionalGetWithoutIsPresent") final var collapsed = collapseManager.createOntology(
+                    toCollapse.get().getOntologyID().getOntologyIRI().get());
 
-			LOGGER.info("Saving collapsed {} to {} ", collapseConfig.ontology(),
-			            collapseConfig.outPath().toFile().getCanonicalFile());
-			collapsed.saveOntology(IRI.create(collapseConfig.outPath().toUri()));
-		}
-	}
+            LOGGER.info("Collapsing {}", collapseConfig.ontology());
+            collapsed.addAxioms(toCollapse.get().axioms(Imports.INCLUDED));
+
+            LOGGER.info("Adding versionInfo to {}", collapseConfig.ontology());
+            final var df = OWLManager.getOWLDataFactory();
+            final var versionAnnotation = df.getOWLAnnotation(df.getOWLVersionInfo(), df.getOWLLiteral(versionInfo));
+            collapsed.getOWLOntologyManager().applyChange(new AddOntologyAnnotation(collapsed, versionAnnotation));
+
+            LOGGER.info("Saving collapsed {} to {} ", collapseConfig.ontology(),
+                    collapseConfig.outPath().toFile().getCanonicalFile());
+            collapsed.saveOntology(IRI.create(collapseConfig.outPath().toUri()));
+        }
+    }
 
 
 }
