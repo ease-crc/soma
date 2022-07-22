184ed3c0884d46abc39051ae817356d826f57a7d
mrnolte <mnolte@uni-bremen.de>
2022-07-22 14:48:38 +0200

    Added the standard rdf_xml (mixed it up with owl_xml before)

--- scripts/java/src/main/java/main/converter/OWLDocumentFormatConverter.java
+++ scripts/java/src/main/java/main/converter/OWLDocumentFormatConverter.java
@@ -3,6 +3,7 @@ package main.converter;
 import org.semanticweb.owlapi.formats.FunctionalSyntaxDocumentFormat;
 import org.semanticweb.owlapi.formats.ManchesterSyntaxDocumentFormat;
 import org.semanticweb.owlapi.formats.OWLXMLDocumentFormat;
+import org.semanticweb.owlapi.formats.RDFXMLDocumentFormat;
 import org.semanticweb.owlapi.model.OWLDocumentFormat;
 import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
 import org.springframework.core.convert.converter.Converter;
@@ -15,7 +16,8 @@ public class OWLDocumentFormatConverter implements Converter<String, OWLDocument
 	@Override
 	public OWLDocumentFormat convert(final String source) {
 		return switch (source.toUpperCase()) {
-			case "XML" -> new OWLXMLDocumentFormat();
+			case "RDF_XML" -> new RDFXMLDocumentFormat();
+			case "OWL_XML" -> new OWLXMLDocumentFormat();
 			case "FUNCTIONAL" -> new FunctionalSyntaxDocumentFormat();
 			case "MANCHESTER" -> new ManchesterSyntaxDocumentFormat();
 			default -> throw new IllegalStateException("Unexpected value: " + source);
