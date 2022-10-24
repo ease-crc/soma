b3799ad83e22b397f3e119ffe814206870f83e95
Mihai Pomarlan <mpomarlan@yahoo.co.uk>
2022-10-24 15:20:42 +0200

    Update konclude script to test object properties.

--- scripts/konclude_test.py
+++ scripts/konclude_test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import argparse
 import re
+import os
 import sys
 from pathlib import Path
 
@@ -38,9 +39,38 @@ def _parseHomebrew(fileText):
         nullClasses.remove(OWL_NOTHING)
     return nullClasses
 
+def makeObjectPropertyQueryFile(objProps, somaPath):
+    queryMap = {}
+    queryOwl = './SOMA_QUERY.owl'
+    with open(queryOwl, 'w') as outfile:
+        outfile.write('Prefix(:=<http://www.ease-crc.org/ont/SOMA_QUERY.owl#>)\n')
+        outfile.write('Prefix(dul:=<http://www.ontologydesignpatterns.org/ont/dul/DUL.owl#>)\n')
+        outfile.write('Prefix(owl:=<http://www.w3.org/2002/07/owl#>)\n')
+        outfile.write('Prefix(rdf:=<http://www.w3.org/1999/02/22-rdf-syntax-ns#>)\n')
+        outfile.write('Prefix(xml:=<http://www.w3.org/XML/1998/namespace>)\n')
+        outfile.write('Prefix(xsd:=<http://www.w3.org/2001/XMLSchema#>)\n')
+        outfile.write('Prefix(rdfs:=<http://www.w3.org/2000/01/rdf-schema#>)\n')
+        outfile.write('Prefix(soma:=<http://www.ease-crc.org/ont/SOMA.owl#>)\n\n\n')
+        outfile.write('Ontology(<http://www.ease-crc.org/ont/SOMA_QUERY.owl>\n')
+        outfile.write('Import(<file://%s>)\n' % somaPath)
+        for k,op in enumerate(objProps):
+            cName = 'http://www.ease-crc.org/ont/SOMA_QUERY.owl#QUERY%d' % k
+            queryMap[cName] = op
+            outfile.write('EquivalentClasses(<%s> ObjectSomeValuesFrom(<%s> owl:Thing))\n' % (cName, op))
+        outfile.write(')\n')
+    return queryMap, queryOwl
+
+def runKonclude(queryOwl, koncludePath):
+    outName = queryOwl[:queryOwl.rfind('.')] + '_OUT.html'
+    os.system("%s classification -i %s -o %s >/dev/null 2>&1" % (koncludePath, queryOwl, outName))
+    return _parseHomebrew(open(outName).read())
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('konclude_output', help='Konclude output file', type=str)
+    parser.add_argument('-k', '--koncludePath', help='path to Konclude executable', default='./.github/actions/Konclude', type=str)
+    parser.add_argument('-s', '--somaPath', help='path to merged SOMA owl', default='./build/SOMA-HOME.owl', type=str)
+    parser.add_argument('-p', '--propertyListPath', help='object property list file', default='', type=str)
     parser.add_argument('-o', '--output', help='output file', default='konclude.html', type=str)
     args = parser.parse_args()
 
@@ -53,6 +83,16 @@ def main():
         'Error': [],
     }
 
+    objProps = []
+    if '' != args.propertyListPath:
+        objProps = [x.strip() for x in open(args.propertyListPath).read().splitlines() if x.strip()]
+    if objProps:
+        query2objectProperty, queryOwl = makeObjectPropertyQueryFile(objProps, args.somaPath)
+        nullQueries = runKonclude(queryOwl, args.koncludePath)
+        for n in nullQueries:
+            if n in query2objectProperty:
+                results['EquivalentObjectProperties'].append(query2objectProperty[n])
+        
     output = format_output(results, format=args.output[args.output.rfind('.'):])
     with open(args.output, 'w') as outfile:
         _ = outfile.write(output)
