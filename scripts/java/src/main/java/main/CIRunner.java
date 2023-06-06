eafc5315048339680f8cce1d0b468beb13e1579b
Ayden <ayden.janssen17@gmail.com>
2023-06-06 17:26:09 +0200

    Add ci runner for IRI namespace checking

--- scripts/java/src/main/java/main/CIRunner.java
+++ scripts/java/src/main/java/main/CIRunner.java
@@ -23,10 +23,13 @@ public class CIRunner implements CommandLineRunner {
 	@Autowired
 	private SubclassNothingRewriter gciRewriter;
 
+	@Autowired
+	private IRINamespaceChecker iriNamespaceChecker;
+
 
 	@Override
 	public void run(final String... args) throws Exception {
-		final CIRunnable[] toRun = {gciRewriter, isDefinedInAdder, versionInfoAdder, collapser, ontologySaver};
+		final CIRunnable[] toRun = {iriNamespaceChecker, gciRewriter, isDefinedInAdder, versionInfoAdder, collapser, ontologySaver};
 		for (final var next : toRun) {
 			next.run();
 		}
